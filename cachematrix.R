ad0e383dd5de992faca0b968416f0f1c0932b71c
asobhan7 <100291622+asobhan7@users.noreply.github.com>
2022-02-23 21:49:39 +0800

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -5,7 +5,7 @@
 
 makeCacheMatrix <- function(x = matrix()) {
 
-}
+} the x is showing a matrix
 
 
 ## Write a short comment describing this function
@@ -13,3 +13,26 @@ makeCacheMatrix <- function(x = matrix()) {
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
 }
+makeVector <- function (x=5) {
+  m<-NULL
+  set <- function (y) {
+    x<<-y 
+    m<<-NULL
+  }
+get<-function()x 
+setmean <-function (mean) m <<-mean 
+getmean <-function()m 
+list(set=set, get=get, 
+     setmean=setmean 
+     getmean=getmean )
+}
+cachemean <-function (x,...)
+  m <-x$getmean()
+if (!is.null(m)) {
+  message('getting cached data')
+  return(m)
+}
+data <-x$get()
+m<-mean(data,.... )
+x$setmean(m)
+m 
